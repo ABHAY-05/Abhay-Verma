fd486140a914188b3262ad7fdd6f076e07e4f2ae
Abhay-05 <abhayvermajune5@gmail.com>
2023-09-05 10:52:26 +0530

    changes

--- src/constants/data.js
+++ src/constants/data.js
@@ -16,7 +16,7 @@ export const github = "https://github.com/ABHAY-05";
 export const linkedin = "https://www.linkedin.com/in/abhay-verma-b3987a210";
 export const email = "Abhayvermajune5@gmail.com";
 export const phone = "+91 8756782835";
-export const resume = "https://drive.google.com/file/d/1GtiIzvuoaKytZqpfRz1QB_NHB3oxvqJQ/view?usp=drive_link";
+export const resume = "https://drive.google.com/file/d/1GY06YXZY-th9vDUKV8e6P1oxZJZ9waIi/view?usp=drive_link";
 
 export const skills = {
     pyhton: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
